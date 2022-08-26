72d0ffe83b231362ad6d14828a6e574526698743
Michael Wood <michaelwood33311@icloud.com>
2022-08-26 11:49:13 -0400

    checking for proper number of parameters

--- createEmailServer.sh
+++ createEmailServer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+clear
 
+if [ "$#" -ne 4 ]; then
+    echo "Please enter: username serverAddress serverName (e.g. root smtp.phishviewer.com phishviewer.com"
+    echo ""
+    exit
+fi
 
 
 ssh $1@$2 'bash -s' < script.sh
